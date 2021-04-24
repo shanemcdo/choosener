3d9d7ed9d7d787417255f3c0be406478cf94ccdf
Shane McDonough <shanemcdman@gmail.com>
2021-04-23 19:36:08 -0700

    Add more styles

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const input_list_item = template.content.firstElementChild
 function append_list_item(){
     let item = input_list_item.cloneNode(true)
     item.addEventListener('keydown', function(event){
+        console.log(event.keyCode);
+        let prev = event.target.parentElement.previousElementSibling?.children[0] || null;
+        let next = event.target.parentElement.nextElementSibling?.children[0] || null;
+        let last = input_list.children[input_list.children.length - 1].children[0] || null;
+        let sibling;
         switch(event.keyCode){
             case 13: // enter key
-                let last = input_list.children[input_list.children.length - 1].children[0];
-                if(event.target.value && last.value)
+                if(event.target.value)
                     append_list_item();
                 break;
             case 8: // backspace
                 if(event.target.value == '' && input_list.children.length > 1){
-                    let sibling = event.target.parentElement.previousElementSibling?.children[0];
-                    if(sibling == null)
-                        sibling = event.target.parentElement.nextElementSibling?.children[0];
+                    sibling = prev || next;
                     input_list.removeChild(event.target.parentElement)
                     if(sibling){
                         sibling.value += ' ';
@@ -23,7 +25,16 @@ function append_list_item(){
                     }
                 }
                 break;
-            return false;
+            case 38: // up arrow
+                if(prev)
+                    prev.focus();
+                break;
+            case 40: // down arrow
+                if(next)
+                    next.focus();
+                else if(event.target.value)
+                    append_list_item();
+                break;
         }
     });
     if(document.activeElement.classList.contains('input-list-input'))
